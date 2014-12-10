affde41970e11d2f990b15bef25aecde2513e072
Patrick Meenan <patmeenan@gmail.com>
2014-12-10 12:58:09 -0500

    Initial Commit

---
+++ xrecord/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xrecord
+//
+//  Created by Patrick Meenan on 12/10/14.
+//  Copyright (c) 2014 WPO Foundation. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
